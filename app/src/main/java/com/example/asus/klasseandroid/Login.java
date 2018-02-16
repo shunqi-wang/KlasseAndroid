65e883abd87aa98fda3dab93945f1d768edb7bb8
anwesha521 <32842260+anwesha521@users.noreply.github.com>
2018-02-16 06:08:22 -0800

    Fixed gradle errors

--- app/src/main/java/com/example/asus/klasseandroid/Login.java
+++ app/src/main/java/com/example/asus/klasseandroid/Login.java
@@ -1,4 +1,4 @@
-package com.example.asus.klasseapp;
+package com.example.asus.klasseandroid;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
--- app/src/main/java/com/example/asus/klasseandroid/Welcome.java
+++ app/src/main/java/com/example/asus/klasseandroid/Welcome.java
@@ -1,4 +1,4 @@
-package com.example.asus.klasseapp;
+package com.example.asus.klasseandroid;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
