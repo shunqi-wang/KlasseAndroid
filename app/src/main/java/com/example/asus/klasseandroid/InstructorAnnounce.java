64e361109a5a60696523181023b48eb1bf705efe
anwesha521 <32842260+anwesha521@users.noreply.github.com>
2018-02-20 09:19:05 -0800

    announcement

---
+++ app/src/main/java/com/example/asus/klasseandroid/InstructorAnnounce.java
@@ -0,0 +1,13 @@
+package com.example.asus.klasseandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstructorAnnounce extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructor_announce);
+    }
+}
---
+++ app/src/main/java/com/example/asus/klasseandroid/StudentAnnounce.java
@@ -0,0 +1,13 @@
+package com.example.asus.klasseandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentAnnounce extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_announce);
+    }
+}
