1bdb303176584323d774892b1e1d96cb8130af5f
anwesha521 <32842260+anwesha521@users.noreply.github.com>
2018-03-10 22:55:56 -0800

    Modularised InstructorAnnounce

--- app/src/main/java/com/example/asus/klasseandroid/InstructorAnnounce.java
+++ app/src/main/java/com/example/asus/klasseandroid/InstructorAnnounce.java
@@ -42,45 +42,49 @@ public class InstructorAnnounce extends AppCompatActivity {
         Button b=findViewById(R.id.postannounce);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                final String a=e.getText().toString();
+            public void onClick(View v) {
+                final String a = e.getText().toString();
                 if (TextUtils.isEmpty(a)) {
                     Toast.makeText(InstructorAnnounce.this, "Please enter message.", Toast.LENGTH_LONG).show();
                 } else {
-                    RequestQueue MyRequestQueue = Volley.newRequestQueue(InstructorAnnounce.this);
-                    StringRequest stringRequest = new StringRequest(Request.Method.POST, HttpURL, new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            if (response.contains("success")) {
-                                Toast.makeText(getApplicationContext(), "Successfully posted!", Toast.LENGTH_SHORT).show();
-                                e.setText("");
-                            }
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
-                            Log.i("anwesha", error.getMessage().toString());
-                        }
-                    }) {
-                        @Override
-                        protected Map<String, String> getParams() {
-                            Map<String, String> params = new HashMap<>();
-                            params.put("professor", name);
-                            params.put("content",a);
-                            params.put("class", room_id + "");
-
-
-                            return params;
-                        }
-                    };
-
-                    MyRequestQueue.add(stringRequest);
+                    postAnnouncement(name, e, a);
                 }
             }
         });
 
 
+    }
+
+    public void postAnnouncement(final String name, final EditText e, final String a)
+    {
+        RequestQueue MyRequestQueue = Volley.newRequestQueue(InstructorAnnounce.this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, HttpURL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.contains("success")) {
+                    Toast.makeText(getApplicationContext(), "Successfully posted!", Toast.LENGTH_SHORT).show();
+                    e.setText("");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
+                Log.i("anwesha", error.getMessage().toString());
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("professor", name);
+                params.put("content",a);
+                params.put("class", room_id + "");
+
+
+                return params;
+            }
+        };
+
+        MyRequestQueue.add(stringRequest);
     }
 }
